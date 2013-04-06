d1f613c067308d1eca40451bf7d83e43cbf3ec59
maxgu <maxgu@t4web.com.ua>
2013-04-06 13:13:56 +0300

    vesrsion fix

--- src/Conserva/Controller/InfoController.php
+++ src/Conserva/Controller/InfoController.php
@@ -7,20 +7,12 @@ use Zend\Version\Version;
 use Zend\Console\ColorInterface as Color;
 use Conserva\Module;
 
-class InfoController extends AbstractActionController
-{
+class InfoController extends AbstractActionController {
 
-    public function versionAction()
-    {
+    public function versionAction() {
         $console = $this->getServiceLocator()->get('console');
 
-        $zf2Path = $this->getZF2Path();
-        if (file_exists($zf2Path . '/Zend/Version/Version.php')) {
-            require_once $zf2Path . '/Zend/Version/Version.php';
-            $msg = 'The application in this folder is using Zend Framework ';
-        } else {
-            $msg = 'The Conserva is using Zend Framework ';
-        }
+        $msg = 'The Conserva is using Zend Framework ';
 
         $console->writeLine(Module::NAME . ' ver. ' . Module::VERSION, Color::GREEN);
         $console->writeLine($msg . Version::VERSION);
